055414e7da3d2e24e5f2d12252dd871dc36702d0
Abhisheke01 <124560403+Abhisheke01@users.noreply.github.com>
2023-03-13 13:45:39 +0530

    Create new01.java

---
+++ new01.java
@@ -0,0 +1,6 @@
+class Add{
+  public static void main (String args[])
+                           {
+                             System.out.printIn("Hello this is from java integration")
+                            }
+}
